8fc57b79dde15ae402f92c403c3e17f9954508d3
Cloud User <ec2-user@ip-172-31-10-217.ap-south-1.compute.internal>
2021-08-05 18:07:04 +0000

    completed project

---
+++ src/main/java/com/vinay/proj1/App.java
@@ -0,0 +1,13 @@
+package com.vinay.proj1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Vinay ga!" );
+    }
+}
